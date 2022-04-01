6a0c959e8ec20871ec6e47d3ada9fe64607914de
Shea Levy <shea@shealevy.com>
2022-04-01 06:23:03 -0400

    build-flake: Wait for cache to be populated

--- actions/plutus-certification/build-flake.nix
+++ actions/plutus-certification/build-flake.nix
@@ -45,9 +45,6 @@
       }];
 
       env.SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-      env.NIX_CONFIG = ''
-        experimental-features = nix-command flakes
-      '';
     }
 
     std.postFact
@@ -73,7 +70,20 @@
 
       # Need to override the cicero-wide setting of post-build-hook since it's not available to the daemon
       export NIX_CONFIG="experimental-features = nix-command flakes"
-      nix build path:. --no-link --json --print-build-logs | jq  '{ ${builtins.toJSON name}: { success: .[0].outputs.out } }' > /local/cicero/post-fact/success/fact
+      res=$(nix build path:. --no-link --json --print-build-logs | jq '.[0].outputs.out')
+      echo "''${res}" | jq  '{ ${builtins.toJSON name}: { success: . } }' > /local/cicero/post-fact/success/fact
+
+      # Wait for the path to be available in the cache
+      for ((i=0;i<8;i++))
+      do
+        if nix path-info --store spongix.service.consul:7745 "''${res}"
+        then
+          break
+        fi
+        let delay=2**i
+        echo "''${res} is not yet in the cache, sleeping for ''${delay} seconds" >&2
+        sleep "''${delay}"
+      done
     '')
   ];
 }
