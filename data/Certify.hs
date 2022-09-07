38028f0a40529621e95ab46c28651de4931f10a8
Shea Levy <shea@shealevy.com>
2022-09-07 15:09:40 -0400

    Workaround certify spamming stdout

--- data/Certify.hs
+++ data/Certify.hs
@@ -4,8 +4,11 @@ module Main where
 import "plutus-contract-certification" Plutus.Contract.Test.Certification.Run
 import "certification" Certification (certification)
 import "base" System.IO
+import "base" GHC.IO.Handle.FD
 
 main :: IO ()
 main = do
   res <- certifyWithOptions (defaultCertificationOptions { certOptOutput = False }) certification
-  hPutStrLn stdout $ certResJSON res
+  h <- fdToHandle 3
+  hPutStrLn h $ certResJSON res
+  hClose h
