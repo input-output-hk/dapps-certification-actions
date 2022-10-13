3186bf83081d816bb59adcd5289216e26caa498c
Shea Levy <shea@shealevy.com>
2022-10-13 09:13:48 -0400

    Point to PR for testing

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Cicero actions driving plutus-certification";
 
-  inputs.cicero.url = "github:input-output-hk/cicero";
+  inputs.cicero.url = "github:shlevy/cicero/api-url";
 
   outputs = { self, nixpkgs, cicero }: let
     inherit (cicero.lib) std;
